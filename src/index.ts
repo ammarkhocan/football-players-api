fc4c0ab138f240978ea5087826839ff32da5ef79
Ammar Ismail Khocan <ammarkhocan02@gmail.com>
2025-09-02 19:56:28 +0700

    refactor: restructure player data

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,26 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+type Players = {
+  id: number;
+  name: string;
+};
 
-export default app
+const players: Players[] = [
+  { id: 1, name: "Luka Modrić" },
+  { id: 2, name: "Kylian Mbappé" },
+  { id: 3, name: "Cristiano Ronaldo" },
+  { id: 4, name: "Vinícius Júnior" },
+  { id: 5, name: "Erling Haaland" },
+];
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.get("/players", (c) => {
+  return c.json(players);
+});
+
+export default app;
