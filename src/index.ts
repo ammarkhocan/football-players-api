b0d950903c34f55fb15f432417ece33c4fe05cd4
Ammar Ismail Khocan <ammarkhocan02@gmail.com>
2025-09-05 10:54:55 +0700

    feat : add endpoint player by id

--- src/index.ts
+++ src/index.ts
@@ -23,4 +23,18 @@ app.get("/players", (c) => {
   return c.json(players);
 });
 
+app.get("/players/:id", (c) => {
+  const id = Number(c.req.param("id"));
+
+  const player = players.find((player) => {
+    return player.id === id;
+  });
+
+  if (!player) {
+    return c.json({ message: "Player not found" }, 404);
+  }
+
+  return c.json(player);
+});
+
 export default app;
